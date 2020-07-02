0a05156f5df47af5f640b7bb72c4fe8456fcc9b5
RAKAN     AL MEZAINI <Rakan-mz@hotmail.com>
2020-07-02 15:27:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@
 
 $(document).ready(function () {
 
-    $('div').on('click', '.closeMenu', function (e) {
-        $('div').removeClass('show');
+    // $('div').on('click', '.closeMenu', function () {
+    //     $('div').removeClass('show');
 
-    })
+    // })
+
+    $("#service").click(function() {
+        $('div').removeClass('show');
+        $("html, body").animate({ scrollTop: $(document).height() }, 2000);
+        return false;
+      });
+
+    $("#us").click(function() {
+        $('div').removeClass('show');
+        $("html, body").animate({ scrollTop: 10 }, 900);
+        return false;
+      });
 
 
     
